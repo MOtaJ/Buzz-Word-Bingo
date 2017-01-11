b459331ecf5eed997afff5768b09273b55fd2693
Mark Ota <markota@Marks-MacBook-Pro-2.local>
2017-01-11 02:10:15 -1000

    BuzzWord Bingo complete

--- app.js
+++ app.js
@@ -2,25 +2,64 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const PORT = process.env.PORT || 3000;
+
 let buzzWordList = {
-  'buzzWords' : []
+  "buzzwords": []
 }
 
-  var urlencodedParser = bodyParser.urlencoded ({ extended: true })
+let score = 0;
+let heard = false;
 
-  app.get('/', (req, res) => {
+console.log(buzzWordList);
+
+  app.use(bodyParser.urlencoded({extended: true}));
+
+  app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/public/index.html')
   })
 
-  app.get('/buzzwords', (req, res) => {
+  app.get('/buzzwords', (req, res, next) => {
     res.send(buzzWordList);
   })
 
-  app.post('/buzzword', urlencodedParser, (req, res) => {
-      buzzWordList.buzzWords.push(req.body);
-      console.log(buzzWordList);
+  app.post('/buzzword', (req, res, next) => {
+      buzzWordList.buzzwords.push(req.body);
       res.send('{"success" : true}');
   })
+
+ app.put('/buzzword', (req, res, next) => {
+  let reqBody = req.body;
+  console.log(reqBody);
+  for(var i = 0; i < buzzWordList.buzzwords.length; i++){
+    if(buzzWordList.buzzwords[i].buzzword === reqBody.buzzword){
+      let currentPoints = parseInt(buzzWordList.buzzwords[i].points);
+      let newPoints = parseInt(reqBody.points);
+      buzzWordList.buzzwords[i].points = currentPoints + newPoints;
+        buzzWordList.buzzwords[i].heard = true;
+      res.send(`{ "success": "true", newScore: ${buzzWordList.buzzwords[i].points} }`);
+      }
+    }
+  });
+
+ app.delete('/buzzword', (req, res, next) => {
+  for(let i = 0; i < buzzWordList.buzzwords.length; i++){
+    if(buzzWordList.buzzwords[i].buzzword === req.body.buzzword){
+      buzzWordList.buzzwords.splice(i, 1);
+      res.send('{"success": true}');
+    }
+  }
+ });
+
+ function reset(){
+  buzzWordList.buzzwords.points = 0;
+  buzzWordList.buzzwords = [];
+ }
+
+ app.post('/reset', (req, res, next) => {
+   reset();
+   res.send('{"success": true}');
+ })
 
 
 var server = app.listen(3000, () => {
